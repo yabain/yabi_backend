d94b1d5cfe85c245a0bdf5440f024f157fb637c1
Flambel SANOU <f.sanou@yaba-in.com>
2025-03-25 12:09:03 +0100

    App controller updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
