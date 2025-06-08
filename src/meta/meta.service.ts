7983fdb1ae54c3cf5f772b2799a9a5bb5030c142
Flambel SANOU <f.sanou@yaba-in.com>
2025-06-08 09:53:47 +0100

    .

--- src/meta/meta.service.ts
+++ src/meta/meta.service.ts
@@ -2,6 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { EventService } from 'src/event/event.service';
 import * as mongoose from 'mongoose';
